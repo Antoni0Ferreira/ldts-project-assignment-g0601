ccfc2a693a9de92b97372cf924eb0eb9892c0f23
FranciscoMaldonado0 <up202004244@up.pt>
2022-01-16 21:58:56 +0000

    Classe abstrata controller e game controller criadas

---
+++ src/main/java/com/ldts/breakout/controller/Controller.java
@@ -0,0 +1,19 @@
+package com.ldts.breakout.controller;
+
+import com.ldts.breakout.Game;
+
+import java.io.IOException;
+
+public abstract class Controller<T> {
+
+    private T model;
+
+    public Controller(T model) {this.model = model;}
+
+    public T getModel() {return model;}
+
+    public void setModel(T model) {this.model = model;}
+
+    public abstract void step(Game game, long time ) throws IOException;
+
+}
