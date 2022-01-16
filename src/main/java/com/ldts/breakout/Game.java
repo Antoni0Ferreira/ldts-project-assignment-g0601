c370a05dbd99dedee96d5dbd7ae5f0fadd4ba332
up202004735 <up202004735@up.pt>
2022-01-16 19:19:10 +0000

    Conflitos de merge #2

--- src/main/java/com/ldts/breakout/Game.java
+++ src/main/java/com/ldts/breakout/Game.java
@@ -1,26 +1,16 @@
 package com.ldts.breakout;
-
-<<<<<<< HEAD
-=======
-
 import com.ldts.breakout.gui.GUI;
 import com.ldts.breakout.gui.LanternaGUI;
 import com.ldts.breakout.state.GameState;
 import com.ldts.breakout.state.MenuState;
 
->>>>>>> d6bfcd12418967e7ff780b4a7e0f615dab24e5bf
 import java.awt.*;
 import java.io.IOException;
-import java.net.URISyntaxException;
-import java.util.Arrays;
-
-import static java.lang.Character.toLowerCase;
-import static java.lang.String.valueOf;
 
 public class Game {
     private GameState gameState;
     private GUI gui;
-    
+
     public Game() throws IOException,FontFormatException{
         gui = new LanternaGUI();
         this.gameState = new MenuState(this, gui);
