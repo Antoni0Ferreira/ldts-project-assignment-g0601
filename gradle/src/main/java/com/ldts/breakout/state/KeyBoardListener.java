848abaa137774add6c76fa82b92938c2fd0511c2
António Ferreira <80832984+up202004735@users.noreply.github.com>
2022-01-16 22:03:34 +0000

    Delete gradle directory

--- gradle/src/main/java/com/ldts/breakout/state/KeyBoardListener.java
+++
@@ -1,7 +0,0 @@
-package com.ldts.breakout.state;
-
-import com.ldts.breakout.gui.GUI;
-
-public interface KeyBoardListener {
-    void keyPressed(GUI.ACTION action);
-}
--- gradle/src/main/java/com/ldts/breakout/viewer/ElementViewer.java
+++
@@ -1,8 +0,0 @@
-package com.ldts.breakout.viewer;
-
-import com.ldts.breakout.gui.GUI;
-import com.ldts.breakout.model.Element;
-
-public interface ElementViewer<T extends Element>{
-    void drawElement(T element, GUI gui);
-}
