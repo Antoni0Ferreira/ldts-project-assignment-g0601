152aecf7b186b94dc9fd51bfb47423f241c1d550
FranciscoMaldonado0 <up202004244@up.pt>
2022-01-17 23:48:22 +0000

    Playing viewer implementado

--- src/main/java/com/ldts/breakout/viewer/state/PlayingViewer.java
+++ src/main/java/com/ldts/breakout/viewer/state/PlayingViewer.java
@@ -1,4 +1,23 @@
 package com.ldts.breakout.viewer.state;
 
-public class PlayingViewer {
+import com.ldts.breakout.gui.GUI;
+import com.ldts.breakout.model.Button;
+import com.ldts.breakout.model.Paddle;
+
+import java.io.IOException;
+import java.util.List;
+
+public class PlayingViewer extends StateViewer {
+    private Paddle paddle;
+
+    public PlayingViewer(GUI gui, List<Button> buttons,Paddle paddle){
+        super(gui, buttons);
+        this.paddle = paddle;
+    }
+
+    @Override
+    public void draw() throws IOException{
+        gui.drawInfo(paddle.getPoints(),paddle.getLives());
+        gui.refresh();
+    }
 }
