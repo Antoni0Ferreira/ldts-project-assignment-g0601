e4f0e4bfbfb012ecd67778a3d9c3c249289a3860
tommygomez25 <maysan54321@gmail.com>
2022-01-18 22:24:11 +0000

    Testes ao endGameState

--- src/test/groovy/com/ldts/breakout/state/EndGameStateTest.groovy
+++ src/test/groovy/com/ldts/breakout/state/EndGameStateTest.groovy
@@ -1,45 +1,49 @@
 package com.ldts.breakout.state
+
 import com.ldts.breakout.Game
-import com.ldts.breakout.controller.MenuController
+import com.ldts.breakout.controller.EndGameController
+import com.ldts.breakout.gui.GUI
 import com.ldts.breakout.gui.KeyBoardObserver
 import com.ldts.breakout.gui.LanternaGUI
+import com.ldts.breakout.model.command.Command
 import org.mockito.Mockito
 
-class MenuStateTest extends spock.lang.Specification{
+class EndGameStateTest extends spock.lang.Specification{
 
     def "Teste Start"(){
         given:
         def game = Mockito.mock(Game.class)
-        def gui = Mockito.mock(LanternaGUI.class)
+        def gui = Mockito.mock(GUI.class)
         def keyBoardObserver = Mockito.mock(KeyBoardObserver.class)
-        def menuState = new MenuState(game,gui)
+        def won = Mockito.anyBoolean()
+        def endGameState = new EndGameState(game,gui,won)
 
         Mockito.when(game.getKeyBoardObserver()).thenReturn(keyBoardObserver)
 
         when:
-        menuState.start()
+        endGameState.start()
 
         then:
         Mockito.verify(keyBoardObserver,Mockito.times(1)).setListener(Mockito.any())
     }
 
-    def "Teste Step"(){
+    def "Teste Step()"(){
         given:
         def game = Mockito.mock(Game.class)
         def gui = Mockito.mock(LanternaGUI.class)
-        def menuController = Mockito.mock(MenuController.class)
         def keyBoardObserver = Mockito.mock(KeyBoardObserver.class)
-        def menuState = new MenuState(game,gui)
+        def endGameController = Mockito.mock(EndGameController.class)
+        def won = Mockito.anyBoolean()
+
+        def endGameState = new EndGameState(game,gui,won)
+        endGameState.setEndGameController(endGameController)
 
         Mockito.when(game.getKeyBoardObserver()).thenReturn(keyBoardObserver)
-        Mockito.when(menuState.getMenuController()).thenReturn(menuController)
+
         when:
-        menuState.step(game)
+        endGameState.step(game)
 
         then:
-        Mockito.verify(menuController,Mockito.times(1)).step()
-
+        Mockito.verify(endGameController,Mockito.times(1)).step()
     }
-
-
 }
